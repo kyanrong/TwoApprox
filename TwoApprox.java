0be87ad10660c9d70f00879518834833dc58eedc
kyanrong <kekyanrong@gmail.com>
2015-03-08 11:13:47 +0800

    Completed version. Improved speed. Added sample inputs and outputs

--- TwoApprox.java
+++ TwoApprox.java
@@ -1,263 +1,248 @@
+// A0083834Y
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Scanner;
-import java.lang.Math;
+import java.util.StringTokenizer;
 
-class TwoApprox {
-	
-	public static void main(String[] args) {
-		
-		// getting input
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		for(int i=0; i<T; i++) {
-			int n = sc.nextInt();
+class TwoApprox { 
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        //long start = System.currentTimeMillis();
+
+        int T = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
+        for(int i=0; i<T; i++) {
+        	br.readLine();
+        	StringTokenizer st = new StringTokenizer(br.readLine());
 			ArrayList<Integer> seq = new ArrayList<Integer>();
-			for(int j=0; j<n; j++) {
-				seq.add(sc.nextInt());
+			while(st.hasMoreTokens()) {
+				seq.add(Integer.parseInt(st.nextToken()));
 			}
 			Permutation p = new Permutation(seq);
 			int dist = twoApprox(p);
-			System.out.println(dist);
+			pw.write(dist+"\n");
 		}
-		sc.close();
-	}
-	
+        
+        //long end = System.currentTimeMillis();
+        //pw.write("time(secs): " + (end-start)/1000.0);
+
+        pw.close();
+        br.close();
+    }
+
 	private static int twoApprox(Permutation p) {
 		int dist = 0;
-		ArrayList<Integer> seq = p.getSeq();
 		
 		if(p.getBreakpoints() == 0) {
 			return 0;
 		}
-
+		
 		if(p.getDecrSeq().size() == 0) {
-			System.out.println("flip increasing - " + p.getIncrSeq().get(0));
-			p.setSeq(flipIncreasing(seq, p.getIncrSeq().get(0)));	
-			System.out.println(p.getSeq());
+			//System.out.println("flip increasing - " + p.getIncrSeq().get(0));
+			p.setSeq(flipIncreasing(p.getSeq(), p.getIncrSeq().get(0)));	
+			dist++;
+			//System.out.println(p.getSeq());
 		}
 		
 		while(p.getBreakpoints() > 0) {
-			int idx_min = seq.indexOf(p.getMin());		
-			int idx_max = seq.indexOf(p.getMax());
+			int idx_min = p.getMinIdx();
+			int idx_max = p.getMaxIdx();
 			int idx_smin = p.getSMinIdx();
 			int idx_smax = p.getSMaxIdx();                    // index of s'min and s'max
 			
-			System.out.println("idx_min: " + idx_min + ", idx_smin: " + idx_smin);
-			System.out.println("idx_max: " + idx_max + ", idx_smax: " + idx_smax);
+			//System.out.println("idx_min: " + idx_min + ", idx_smin: " + idx_smin);
+			//System.out.println("idx_max: " + idx_max + ", idx_smax: " + idx_smax);
 			
-			//1st condition guarantees there will be a decreasing strip
-			//2nd condition need to check if there exists a decreasing strip after flipping
-			//lookaheadOK(p, idx_min, idx_smin
-			if((idx_min<idx_smin) || (idx_smin<idx_min && lookaheadOK(p, idx_smin, idx_min))) {
-				System.out.println("flip min - " + p.getMin());	
-				p.setSeq(flip(seq, idx_min, idx_smin));
-			}
-			//lookaheadOK(p, idx_max, idx_smax
-			else if(idx_smax<idx_max || (idx_max<idx_smax && lookaheadOK(p, idx_max, idx_smax))) {
-				System.out.println("flip max - " + p.getMax());
-				p.setSeq(flip(seq, idx_max, idx_smax));
-			}
-			else {
-				System.out.println("flip min max - " + p.getMin() + " " + p.getMax());
-				p.setSeq(flipMinMax(seq, idx_min, idx_max));
-				System.out.println(seq);
-			}
-			
-			if(p.getBreakpoints() == 0) {
+			// first condition guarantees there will be a decreasing strip
+			if((idx_min<idx_smin) || lookaheadOK(p.getSeq(), "min", idx_smin, idx_min)) {
+				//System.out.println("flip min - " + p.getMin());	
+				p.setSeq(flip(p.getSeq(), "min", idx_min, idx_smin));
 				dist++;
-				break;
-			}
-			if(p.getDecrSeq().size() == 0) {
-				System.out.println("flip increasing - " + p.getIncrSeq().get(0));
-				p.setSeq(flipIncreasing(seq, p.getIncrSeq().get(0)));
+				//System.out.println(p.getSeq());
+				//System.out.println("---------------------------------------------------------");
 			}
 			
-			dist++;
-			System.out.println(seq);
-			System.out.println("---------------------------------------------------------");
+			else if(idx_smax<idx_max || lookaheadOK(p.getSeq(), "max", idx_max, idx_smax)) {
+				//System.out.println("flip max - " + p.getMax());
+				p.setSeq(flip(p.getSeq(), "max", idx_max, idx_smax));
+				dist++;
+				//System.out.println(p.getSeq());
+				//System.out.println("---------------------------------------------------------");
+			}
+			
+			else {
+				//System.out.println("flip min max - " + p.getMin() + " " + p.getMax());
+				p.setSeq(flipMinMax(p.getSeq(), idx_min, idx_max));
+				dist++;
+				//System.out.println(p.getSeq());
+				//System.out.println("---------------------------------------------------------");
+				if(p.getBreakpoints()>0) {
+					//System.out.println("flip increasing - " + p.getIncrSeq().get(0));
+					p.setSeq(flipIncreasing(p.getSeq(), p.getIncrSeq().get(0)));
+					dist++;
+					//System.out.println(p.getSeq());
+					//System.out.println("---------------------------------------------------------");
+				}
+			}
 		}
 		
 		return dist;
 	}
-	
-	// idx_a < idx_b
-	private static boolean lookaheadOK(Permutation p, int idx_a, int idx_b) {
-		int start, end;
+
+	private static ArrayList<Integer> flipMinMax(ArrayList<Integer> seq, int idx_min, int idx_max) {
+		if(idx_min < idx_max) {
+			Collections.reverse(seq.subList(idx_min, idx_max+1));
+		}
+		else {
+			Collections.reverse(seq.subList(idx_max, idx_min+1));
+		}
+		return seq;
+	}
+
+	private static ArrayList<Integer> flip(ArrayList<Integer> seq, String type, int idx_m, int idx_sm) {
+		if(type.equals("min")) {
+			if(idx_m < idx_sm) {
+				Collections.reverse(seq.subList(idx_m+1, idx_sm+1));
+			}
+			else {
+				Collections.reverse(seq.subList(idx_sm+1, idx_m+1));
+			}
+		}
+		else {
+			if(idx_m < idx_sm) {
+				Collections.reverse(seq.subList(idx_m, idx_sm));
+			}
+			else {
+				Collections.reverse(seq.subList(idx_sm, idx_m));
+			}
+		}
 		
-		start = idx_a;
-		end = idx_b;
+		return seq;
+	}
+
+	//idx_a < idx_b
+	private static boolean lookaheadOK(ArrayList<Integer> seq, String type, int idx_a, int idx_b) {
+		ArrayList<Integer> temp = new ArrayList<Integer>();
+		temp.addAll(seq);
+
+		if(type.equals("min")) {
+			Collections.reverse(temp.subList(idx_a+1, idx_b+1));
+		}
 		
-		// if there still exists at least 1 decreasing strip after flipping
-		if(p.getDecrSeq().size()-1 > 0) {
-			System.out.println("here");
+		else {
+			Collections.reverse(temp.subList(idx_a, idx_b));
+		}
+		
+		if(hasDecreasing(temp)) {
 			return true;
 		}
-
-		ArrayList<ArrayList<Integer>> incr = p.getIncrSeq();
-		// if there exists an increasing strip within start and end,
-		// which will become a decreasing strip after flipping
-		for(int i=0; i<incr.size(); i++) {
-			int ele = incr.get(i).get(0);
-			if(p.getSeq().indexOf(ele) > start && p.getSeq().indexOf(ele) < end) {
-				System.out.println("here 2");
-				return true;
-			}
-		}
-			
-		if(end+1 < p.getSize()) {
-			if(p.getSeq().get(start+1) - p.getSeq().get(end+1) > 0) {
-				System.out.println("here 3");
-				return true;
-			}
-		}
-			
-		System.out.println("here 4");
 		return false;
 	}
-	
-	private static ArrayList<Integer> flip(ArrayList<Integer> seq, int idx_m, int idx_sm) {
-		int j = 0;
-		ArrayList<Integer> temp;
-		
-		if(idx_m < idx_sm) {
-			temp = new ArrayList<Integer>(seq.subList(idx_m+1, idx_sm+1));
-			Collections.reverse(temp);
-			for(int i=idx_m+1; i<idx_sm+1; i++) {
-				seq.set(i, temp.get(j));
-				j++;
-			}
-		}
-		else {
-			temp = new ArrayList<Integer>(seq.subList(idx_sm+1, idx_m+1));
-			Collections.reverse(temp);
-			for(int i=idx_sm+1; i<idx_m+1; i++) {
-				seq.set(i, temp.get(j));
-				j++;
-			}
-		}
-		
+
+	private static ArrayList<Integer> flipIncreasing(ArrayList<Integer> seq, Pair strip) {
+		Collections.reverse(seq.subList(strip.getStart(), strip.getEnd()));
 		return seq;
 	}
 	
-	private static ArrayList<Integer> flipMinMax(ArrayList<Integer> seq, int idx_min, int idx_max) {
-		int j = 0;
-		ArrayList<Integer> temp;
-		
-		if(idx_min < idx_max) {
-			temp = new ArrayList<Integer>(seq.subList(idx_min, idx_max+1));
-			Collections.reverse(temp);
-			for(int i=idx_min; i<idx_max+1; i++) {
-				 seq.set(i, temp.get(j));
-				 j++;
-			 }
+	private static boolean hasDecreasing(ArrayList<Integer> seq) {
+		int[] diff = new int[seq.size()-1];
+		for(int i=0; i<diff.length; i++) {
+			diff[i] = seq.get(i+1) - seq.get(i);
 		}
-		else {
-			temp = new ArrayList<Integer>(seq.subList(idx_max, idx_min+1));
-			Collections.reverse(temp);
-			for(int i=idx_max; i<idx_min+1; i++) {
-				 seq.set(i, temp.get(j));
-				 j++;
-			 }
+		int start = 0;
+		for(int j=0; j<diff.length; j++) {
+			// if is a continuous strip
+			if(Math.abs(diff[j])==1 && diff[j]==diff[start]) {
+				continue;
+			}
+			if(start > 0) {
+				if(diff[start]!=1) {
+					return true;
+				}
+			}
+			start = j+1;
 		}
-		return seq;
-	}
-	
-	private static ArrayList<Integer> flipIncreasing(ArrayList<Integer> seq, ArrayList<Integer> strip) {
-		int j = seq.indexOf(strip.get(0));
-		Collections.reverse(strip);
-		for(int i=0; i<strip.size(); i++) {
-			seq.set(j, strip.get(i));
-			j++;
-		}
-		return seq;
+		return false;
 	}
 }
 
 class Permutation {
 	
 	int size, min, max, breakpoints;
+	int min_idx, max_idx;
 	ArrayList<Integer> seq;
-	ArrayList<Integer> identity;
-	ArrayList<ArrayList<Integer>> incr;
-	ArrayList<ArrayList<Integer>> decr;
+	ArrayList<Pair> incr;
+	ArrayList<Pair> decr;
 	
-	@SuppressWarnings("unchecked")
 	Permutation(ArrayList<Integer> input) {
 		seq = input;
 		seq.add(seq.size()+1);
 		seq.add(0, 0);
 		size = seq.size();
-		identity = (ArrayList<Integer>) seq.clone();
-		Collections.reverse(identity);
 		
 		update();
 	}
-		
+	
 	private void update() {
-		incr = new ArrayList<ArrayList<Integer>>();
-		decr = new ArrayList<ArrayList<Integer>>();
+		incr = new ArrayList<Pair>();
+		decr = new ArrayList<Pair>();
 		min = size;
 		max = 0;
 		breakpoints = 0;
+		min_idx = max_idx = -1;
 		
-		getBreakpointsAndStrips();
-		getMinAndMax();
+		preprocess();
 		
 		/*System.out.println("---------------------------------------------------------");
 		System.out.println("breakpoints: " + breakpoints); 
 		System.out.print("increasing, "); printSeq(incr); 
 		System.out.print("decreasing, "); printSeq(decr); 
-		System.out.println("min: " + min + ", max: " + max);*/
-		
+		System.out.println("min: " + min + ", max: " + max); 
+		System.out.println("min idx: " + min_idx + ", max_idx: " + max_idx);*/		
 	}
 	
-	private void getBreakpointsAndStrips() {
-		ArrayList<Integer> temp = new ArrayList<Integer>();
-		temp.add(seq.get(0));
-		
-		for(int i=0; i<size-1; i++) {
-			if(Math.abs(seq.get(i)-seq.get(i+1)) != 1) {
+	private void preprocess() {
+		int[] diff = new int[size-1];
+		for(int i=0; i<diff.length; i++) {
+			diff[i] = seq.get(i+1) - seq.get(i);
+		}
+		int start = 0;
+		for(int j=0; j<diff.length; j++) {
+			// if is a continuous strip
+			if(Math.abs(diff[j])==1 && diff[j]==diff[start]) {
+				continue;
+			}
+			if(start > 0) {
+				if(diff[start]==1) {
+					incr.add(new Pair(start, j+1));
+				}
+				else {
+					decr.add(new Pair(start, j+1));
+					findMinMax(start, j+1);
+				}
 				breakpoints++;
-				sortStrip(temp);
-				temp = new ArrayList<Integer>();
-				temp.add(seq.get(i+1));
 			}
-			else {
-				temp.add(seq.get(i+1));
-			}
-		}
-		
-		sortStrip(temp);
-	} 
-	
-	private void sortStrip(ArrayList<Integer> strip) {
-		if(strip.contains(0) || strip.contains(size-1)) {
-			return;
-		}
-		if(strip.get(0) < strip.get(strip.size()-1)) {
-			incr.add(strip);
-		}
-		else {
-			decr.add(strip);
+			start = j+1;
 		}
 	}
 	
-	// excludes 0 and n+1
-	private void getMinAndMax() {
-		for(int i=0; i<decr.size(); i++) {
-			Object min_temp = Collections.min(decr.get(i));
-			Object max_temp = Collections.max(decr.get(i));
-			if((Integer)min_temp<min && (Integer)min_temp!=0) min = (Integer) min_temp;
-			if((Integer)max_temp>max && (Integer)max_temp!=size-1) max = (Integer) max_temp;
+	private void findMinMax(int start, int end) {
+		int min_temp = seq.get(end-1);
+		int max_temp = seq.get(start);
+		if(min_temp<min && min_temp!=0) {
+			min = min_temp;
+			min_idx = end-1;
 		}
-	}
-	
-	@SuppressWarnings("unused")
-	private void printSeq(ArrayList<ArrayList<Integer>> s) {
-		System.out.println("size: " + s.size());
-		for(int i=0; i<s.size(); i++) {
-			System.out.println(s.get(i));
+		if(max_temp>max && max!=size-1) {
+			max = max_temp;
+			max_idx = start;
 		}
 	}
 	
@@ -269,16 +254,20 @@ class Permutation {
 		return breakpoints;
 	}
 	
-	public ArrayList<Integer> getIdentity() {
-		return identity;
-	}
-	
 	public int getMin() {
 		return min;
 	}
 	
 	public int getMax() {
 		return max;
+	}
+	
+	public int getMinIdx() {
+		return min_idx;
+	}
+	
+	public int getMaxIdx() {
+		return max_idx;
 	}
 	
 	public int getSMinIdx() {
@@ -293,11 +282,11 @@ class Permutation {
 		return seq;
 	}
 	
-	public ArrayList<ArrayList<Integer>> getDecrSeq() {
+	public ArrayList<Pair> getDecrSeq() {
 		return decr;
 	}
 	
-	public ArrayList<ArrayList<Integer>> getIncrSeq() {
+	public ArrayList<Pair> getIncrSeq() {
 		return incr;
 	}
 	
@@ -306,4 +295,24 @@ class Permutation {
 		update();
 	}
 
+}
+
+// start and end indexes of a strip
+class Pair {
+	
+	private int start;
+	private int end;			// end index is exclusive
+	
+	public Pair(int s, int e) {
+		start = s;
+		end = e;
+	}
+	
+	public int getStart() {
+		return start;
+	}
+	
+	public int getEnd() {
+		return end;
+	}
 }
